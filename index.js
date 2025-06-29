02244d549d6007028fea5dc868d8ba21df636220
rakes singh <rakeshdevcode@gmail.com>
2025-06-29 23:58:22 +0530

    update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-  let SecondLargestNumber = (arr) => {
-    let largest = arr[0];
-    let secondLargest=arr[0];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > largest) {
-        secondLargest=largest;
-        largest = arr[i];
-      }
-    }
-    return secondLargest;
-  };
+let SecondLargestNumber = (arr) => {
+  if (arr.length < 2 || arr.length < 1 ) return "No second largest.....";
 
-  let arr = [99,3, 4, 5, 6, 7, 8, 9, 10, -5, -10, 11,50,100,101];
-  let result=SecondLargestNumber(arr);
-  console.log(result);
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest === -Infinity ? "No second Largest" : secondLargest;
+};
+
+let arr = [];
+let result = SecondLargestNumber(arr);
+console.log(result);
