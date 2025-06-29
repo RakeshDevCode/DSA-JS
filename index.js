e89edb6e43a07ae2b99a4411d160532e1342d744
rakes singh <rakeshdevcode@gmail.com>
2025-06-29 21:08:41 +0530

    first  code

---
+++ index.js
@@ -0,0 +1,14 @@
+// Write  a function that return Largest no in the Array
+
+let largestNumber = (arr) => {
+  let largest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  console.log(largest);
+};
+
+let arr = [3, 4, 5, 6, 7, 8, 9, 10, -5, -10, 11, 100];
+largestNumber(arr);
