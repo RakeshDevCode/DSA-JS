2adcddb78a032f938b029eff48d4fb440c0d2728
rakes singh <rakeshdevcode@gmail.com>
2025-06-29 23:11:51 +0530

    update new dsa questions and answers

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// Write  a function that return Largest no in the Array
-
-let largestNumber = (arr) => {
-  let largest = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      largest = arr[i];
+  let SecondLargestNumber = (arr) => {
+    let largest = arr[0];
+    let secondLargest=arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        secondLargest=largest;
+        largest = arr[i];
+      }
     }
-  }
-  console.log(largest);
-};
+    return secondLargest;
+  };
 
-let arr = [3, 4, 5, 6, 7, 8, 9, 10, -5, -10, 11, 100];
-largestNumber(arr);
-
-
-
-
+  let arr = [99,3, 4, 5, 6, 7, 8, 9, 10, -5, -10, 11,50,100,101];
+  let result=SecondLargestNumber(arr);
+  console.log(result);
